888f195b32a20573ee2885d2502d2d5deb263571
SkyNhiNguyen <nnguyen9@hawaii.edu>
2016-10-31 22:10:54 -1000

    +carousel

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,4 @@
+// TOGGLE NAV========================
 $(function() {
   var $navToggle = $('.toggle-nav');
   var $mainNav = $('.mainnav ul');
@@ -15,4 +16,44 @@ $(function() {
       $navToggle.removeClass('active');
     }
   });
+});
+
+
+// LINEUP CAROUSEL=====================
+$('.carousel-lineup').slick({
+  nextArrow: '<i class="fa fa-arrow-right"></i>',
+  prevArrow: '<i class="fa fa-arrow-left"></i>',
+  dots: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 });
