305fb4d310cdc15f43af808c109293ea179b612e
SkyNhiNguyen <nnguyen9@hawaii.edu>
2016-10-31 22:33:18 -1000

    continued carouse

--- src/scripts.js
+++ src/scripts.js
@@ -34,8 +34,8 @@ $('.carousel-lineup').slick({
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToShow: 4,
+        slidesToScroll: 4,
         infinite: true,
         dots: true
       }
@@ -43,7 +43,7 @@ $('.carousel-lineup').slick({
     {
       breakpoint: 600,
       settings: {
-        slidesToShow: 2,
+        slidesToShow: 3,
         slidesToScroll: 2
       }
     },
